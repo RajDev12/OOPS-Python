9d773e1d30d1ec9aacdab9a972ff4f2d1dd85c4c
rajdev12 <rjnath98@gmail.com>
2024-03-31 02:31:15 +0530

    super keyword uses demo

---
+++ Super-KeyWord.py
@@ -0,0 +1,20 @@
+# super keyoword is used to import parent class data and used it in  child class
+class Phone:
+    def __init__(self, price, brand, camera):
+        print ("Inside phone constructor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    def buy(self):
+        print ("Buying a phone")
+
+class SmartPhone(Phone):
+    def buy(self):
+        print ("Buying a smartphone")
+        # syntax to call parent ka buy method
+        super().buy()
+
+s=SmartPhone(20000, "Apple", 13)
+
+s.buy()
