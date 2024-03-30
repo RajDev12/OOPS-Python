040dbbb470b176f6d15a3854ea10c931dcc32c4c
rajdev12 <rjnath98@gmail.com>
2024-03-31 02:17:17 +0530

    constructor overriding

---
+++ Constructor-overridng.py
@@ -0,0 +1,27 @@
+# Example
+
+# parent
+class User:
+
+  def __init__(self):
+    self.name = 'nitish'
+    self.gender = 'male'
+
+  def login(self):
+    print('login')
+
+# child
+class Student(User):
+
+  def __init__(self):
+    self.rollno = 100
+
+  def enroll(self):
+    print('enroll into the course')
+
+u = User()
+s = Student()
+
+print(s.name)
+s.login()
+s.enroll()
