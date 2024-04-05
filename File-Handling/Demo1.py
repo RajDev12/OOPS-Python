5da16eb7ff2f17c4485259537a8e8464fcd7ef63
rajdev12 <rjnath98@gmail.com>
2024-04-05 16:56:41 +0530

    use of readline

--- File-Handling/Demo1.py
+++ File-Handling/Demo1.py
@@ -34,6 +34,49 @@
 
 
 #Introducing append mode to not erase old content from the file
-f=open("File-Handling\sample1.txt", 'a')
-f.write('Use append to insert at the end of old content')
-f.close()
+# f=open("File-Handling\sample1.txt", 'a')
+# f.write('Use append to insert at the end of old content')
+# f.close()
+
+
+##Passing  a multiple lines inside a list
+L=['hi\n','how are you\n','what the hell']
+# f=open('File-Handling\sample.txt','w')
+# f.writelines(L)
+# f.close()
+
+
+##Reading from a file
+# f=open('File-Handling\sample.txt', 'r')
+# s=f.read()
+# print(s)
+# f.close()
+
+##Readin upto n characters from a file
+# f=open('File-Handling\sample.txt', 'r')
+# s=f.read(10)
+# print(s)
+# print(f.read(5)) ##it will start reading from 11th character as char till 10 already read
+# f.close()
+
+##Reading one by one line using readline()
+# f=open('File-Handling\sample.txt','r')
+# print(f.readline(),end="    ") ##end content will show after each line is printed
+# print(f.readline(),end='    ')
+# f.close()
+
+##Reading  whole file using readline()
+f=open('C:\\Users\Dev_nath\OneDrive\Desktop\PROJECTS\OOPS-Python\File-Handling\sample.txt','r')
+while True:
+    data=f.readline()
+    if data==' ':
+        break
+    else:
+        print(data,end=' ')
+    f.close()
+
+
+
+
+
+
