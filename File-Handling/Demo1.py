3e7f27df26f2da5b10747d3b8a5e4b76f8e64af1
rajdev12 <rjnath98@gmail.com>
2024-04-10 12:30:06 +0530

    with context manager

--- File-Handling/Demo1.py
+++ File-Handling/Demo1.py
@@ -17,6 +17,8 @@
 # f.write('Stop the writing and close')
 # f.close()
 # # f.write("cant Write into file after closing the file")
+
+
 # #write multiples line =if the file is NOT present
 # f=open("sample1.txt", 'w')
 # f.write('Hi how are you?')   #15=RETURNS THE NUMBER OF CHARACTER INSERTED
@@ -40,15 +42,15 @@
 
 
 ##Passing  a multiple lines inside a list
-L=['hi\n','how are you\n','what the hell']
+L=['hi\n','how are you\n','what the hell'] #if we dont give the \n then it willl print all the ellement of list in t
 # f=open('File-Handling\sample.txt','w')
 # f.writelines(L)
-# f.close()
+# f.close() 
 
 
 ##Reading from a file
-# f=open('File-Handling\sample.txt', 'r')
-# s=f.read()
+# f=open('sample.txt', 'r')
+# s=f.read()  #it wil read all the lines from the file.
 # print(s)
 # f.close()
 
@@ -56,7 +58,7 @@ L=['hi\n','how are you\n','what the hell']
 # f=open('File-Handling\sample.txt', 'r')
 # s=f.read(10)
 # print(s)
-# print(f.read(5)) ##it will start reading from 11th character as char till 10 already read
+# print(f.read(5)) #=>it will start reading from 11th character as char till 10 already read
 # f.close()
 
 ##Reading one by one line using readline()
@@ -66,15 +68,21 @@ L=['hi\n','how are you\n','what the hell']
 # f.close()
 
 ##Reading  whole file using readline()
-f=open('C:\\Users\Dev_nath\OneDrive\Desktop\PROJECTS\OOPS-Python\File-Handling\sample.txt','r')
-while True:
-    data=f.readline()
-    if data==' ':
-        break
-    else:
-        print(data,end=' ')
-    f.close()
+# f=open('C:\\Users\Dev_nath\OneDrive\Desktop\PROJECTS\OOPS-Python\File-Handling\sample.txt','r')
+# while True:
+#     data=f.readline()
+#     if data==' ':
+#         break
+#     else:
+#         print(data,end=' ')
+#     f.close()
 
+
+
+##Using Context Manager(With) to open a file
+with open('sample.txt','w') as f:
+    f.write("with open")
+    
 
 
 
